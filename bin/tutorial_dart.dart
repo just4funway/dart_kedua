54011913370245f1c66da19fb87b146fce2ab132
just4funway <s32210077@student.ubm.ac.id>
2023-09-15 10:32:07 +0700

    commit ke 2

---
+++ bin/tutorial_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tutorial_dart/tutorial_dart.dart' as tutorial_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutorial_dart.calculate()}!');
+}
---
+++ test/tutorial_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutorial_dart/tutorial_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
